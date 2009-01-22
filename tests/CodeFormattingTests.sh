2c884ffa33aab0d3ec3dfae06813e9d79c272722
inghamn <inghamn@293550cc-23b7-11df-9cb1-5138f70e336d>
2009-01-22 15:39:16 +0000

    The code formatting test script now only checks inside classes and HTML
    git-svn-id: https://php-blossom.googlecode.com/svn/trunk@166 293550cc-23b7-11df-9cb1-5138f70e336d

--- tests/CodeFormattingTests.sh
+++ tests/CodeFormattingTests.sh
@@ -6,6 +6,6 @@ for FILE in `ls ../classes/`;
 	do $PHPCS --standard=$STANDARD ../classes/$FILE;
 done;
 
-#for FILE in `find ../html/ -name '*.php'`;
-#	do $PHPCS --standard=$STANDARD $FILE;
-#done
+for FILE in `find ../html/ -name '*.php'`;
+	do $PHPCS --standard=$STANDARD $FILE;
+done
