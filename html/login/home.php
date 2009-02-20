9c71dd3a764a10751c03b7d5e2111e4faa7e9204
inghamn <inghamn@293550cc-23b7-11df-9cb1-5138f70e336d>
2009-02-20 20:51:35 +0000

    Login is now in its own directory and shown in the menubar
    git-svn-id: https://php-blossom.googlecode.com/svn/trunk@174 293550cc-23b7-11df-9cb1-5138f70e336d

---
+++ html/login/home.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright 2009 City of Bloomington, Indiana
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+$template = new Template();
+$template->blocks[] = new Block('loginForm.inc');
+echo $template->render();
