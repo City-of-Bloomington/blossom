75dc502fd98a2173a8bf50e28abb4740150b8f17
inghamn <inghamn@293550cc-23b7-11df-9cb1-5138f70e336d>
2009-05-08 13:25:16 +0000

    Configuration now uses a single DSN for the database connection.  Makes it more in line with usage for databases other than MySQL
    git-svn-id: https://php-blossom.googlecode.com/svn/trunk@196 293550cc-23b7-11df-9cb1-5138f70e336d

--- libraries/framework/classes/Database.php
+++ libraries/framework/classes/Database.php
@@ -21,9 +21,7 @@ class Database
 		}
 		if (!self::$pdo) {
 			try {
-				self::$pdo = new PDO(DB_TYPE.':'.DB_DSN.'dbname='.DB_NAME,
-										DB_USER,
-										DB_PASS,
+				self::$pdo = new PDO(DB_DSN,DB_USER,DB_PASS,
 										array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 			}
 			catch (PDOException $e) {
