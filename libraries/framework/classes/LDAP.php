ceb297399aa29d3d580012d7b45e720ae1375f95
inghamn <inghamn@293550cc-23b7-11df-9cb1-5138f70e336d>
2011-02-11 18:01:23 +0000

    Updated copyright
    git-svn-id: https://php-blossom.googlecode.com/svn/trunk@259 293550cc-23b7-11df-9cb1-5138f70e336d

--- libraries/framework/classes/LDAP.php
+++ libraries/framework/classes/LDAP.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2008 City of Bloomington, Indiana
+ * @copyright 2008-2011 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
