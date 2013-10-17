28337a12c52103d7f69b68caf0e22a22cf229e29
inghamn <inghamn@293550cc-23b7-11df-9cb1-5138f70e336d>
2013-10-17 18:04:09 +0000

    Added error checking for Person loading.
    git-svn-id: https://php-blossom.googlecode.com/svn/trunk@339 293550cc-23b7-11df-9cb1-5138f70e336d

--- controllers/PeopleController.php
+++ controllers/PeopleController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2012 City of Bloomington, Indiana
+ * @copyright 2012-2013 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
@@ -18,17 +18,29 @@ class PeopleController extends Controller
 	{
 		try {
 			$person = new Person($_REQUEST['person_id']);
+			$this->template->blocks[] = new Block('people/personInfo.inc',array('person'=>$person));
 		}
 		catch (Exception $e) {
 			$_SESSION['errorMessages'][] = $e;
 		}
 
-		$this->template->blocks[] = new Block('people/personInfo.inc',array('person'=>$person));
 	}
 
 	public function update()
 	{
-		$person = isset($_REQUEST['person_id']) ? new Person($_REQUEST['person_id']) : new Person();
+		if (isset($_REQUEST['person_id']) && $_REQUEST['person_id']) {
+			try {
+				$person = new Person($_REQUEST['person_id']);
+			}
+			catch (Exception $e) {
+				$_SESSION['errorMessages'][] = $e;
+				header("Location: $errorURL");
+				exit();
+			}
+		}
+		else {
+			$person = new Person();
+		}
 
 		if (isset($_POST['firstname'])) {
 			$person->handleUpdate($_POST);
