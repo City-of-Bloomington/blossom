c653209a9ac9c7534c9d6aa270b07130911caee2
Cliff Ingham <inghamn@bloomington.in.gov>
2014-07-28 09:06:34 -0400

    Added comment for PeopleTable::find()

--- Models/PeopleTable.php
+++ Models/PeopleTable.php
@@ -13,6 +13,12 @@ class PeopleTable extends TableGateway
 {
 	public function __construct() { parent::__construct('people', __namespace__.'\Person'); }
 
+	/**
+	 * @param array $fields
+	 * @param string|array $order Multi-column sort should be given as an array
+	 * @param bool $paginated Whether to return a paginator or a raw resultSet
+	 * @param int $limit
+	 */
 	public function find($fields=null, $order='lastname', $paginated=false, $limit=null)
 	{
 		$select = new Select('people');
