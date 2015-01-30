7c270564d30f7f7665a1c9e760b04182a94fe08f
inghamn <inghamn@bloomington.in.gov>
2015-01-30 15:53:26 -0500

    Ignored compiled MO files

--- build.sh
+++ build.sh
@@ -12,6 +12,11 @@ if [ ! -d $DIST ]
 	then mkdir $DIST
 fi
 
+# Compile the Lanague files
+cd $DIR/language
+./build_lang.sh
+cd $DIR
+
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
 rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
