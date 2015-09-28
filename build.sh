d1659d8ef4e9d9af6f29502cdeb2d78ecbdaa297
inghamn <inghamn@bloomington.in.gov>
2015-09-28 11:22:58 -0400

    Updated build script for composer libraries

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-RELEASE_NAME=Blossom
+APPNAME=blossom
 DIR=`pwd`
 BUILD=$DIR/build
-DIST=$DIR/dist
 
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
-fi
-
-if [ ! -d $DIST ]
-	then mkdir $DIST
 fi
 
 # Compile the Lanague files
@@ -17,9 +12,13 @@ cd $DIR/language
 ./build_lang.sh
 cd $DIR
 
+# Compile the SASS
+cd $DIR/public/css
+./build_css.sh
+cd $DIR
+
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
-rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
-
-# Create a distribution tarball of the build
-tar czvf $DIST/$RELEASE_NAME.tar.gz --transform=s/build/$RELEASE_NAME/ build
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/$APPNAME
+cd $BUILD
+tar czvf $APPNAME.tar.gz $APPNAME
