7f24770a38af41dd27202abe352e7ff90c6487f1
Dan Hiester <hiesterd@bloomington.in.gov>
2016-04-28 12:33:33 -0400

    Whitespace adjustment.

--- public/js/menus.js
+++ public/js/menus.js
@@ -4,23 +4,23 @@
         var openLaunchers = document.querySelectorAll('.dropdown [aria-expanded="true"]'),
             len = openLaunchers.length,
             i   = 0;
-            for (i=0; i<len; i++) {
-                openLaunchers[i].setAttribute("aria-expanded", "false");
-            }
-        },
-        launcherClick = function(e) {
-            var launcher      = e.target;
-            var menu          = launcher.parentElement.querySelector('.dropdown .links');
-            e.preventDefault();
-            launcher.blur();
-            closeMenus();
-            launcher.setAttribute("aria-expanded", "true");
-            e.stopPropagation();
-            menu.focus();
-        },
-        launchers = document.querySelectorAll('.dropdown .launcher'),
-        len   = launchers.length,
-        i = 0;
+        for (i=0; i<len; i++) {
+            openLaunchers[i].setAttribute("aria-expanded", "false");
+        }
+    },
+    launcherClick = function(e) {
+        var launcher      = e.target;
+        var menu          = launcher.parentElement.querySelector('.dropdown .links');
+        e.preventDefault();
+        launcher.blur();
+        closeMenus();
+        launcher.setAttribute("aria-expanded", "true");
+        e.stopPropagation();
+        menu.focus();
+    },
+    launchers = document.querySelectorAll('.dropdown .launcher'),
+    len   = launchers.length,
+    i = 0;
 
     for (i=0; i<len; i++) {
         launchers[i].addEventListener('click', launcherClick);
