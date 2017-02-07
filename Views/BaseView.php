91718384cf4c9d0828fe5129c961eb2aaf502b2b
inghamn <inghamn@bloomington.in.gov>
2017-02-07 10:00:52 -0500

    Twig files are now named file.$format.twig

--- Views/BaseView.php
+++ Views/BaseView.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2016 City of Bloomington, Indiana
+ * @copyright 2016-2017 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  */
 namespace Application\Views;
--- Views/IndexView.php
+++ Views/IndexView.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2016 City of Bloomington, Indiana
+ * @copyright 2016-2017 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  */
 namespace Application\Views;
@@ -9,6 +9,7 @@ class IndexView extends BaseView
 {
     public function render()
     {
-        echo $this->twig->render('index.html', $this->vars);
+        $format = !empty($_REQUEST['format']) ? $_REQUEST['format'] : 'html';
+        echo $this->twig->render("index.$format.twig", $this->vars);
     }
 }
