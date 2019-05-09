8985250c78bf9b2f1f23024263313da3a1575eed
inghamn <inghamn@bloomington.in.gov>
2019-05-09 15:43:30 -0400

    Added start of phpunit test suites

---
+++ src/Test/Unit/test_config.php
@@ -0,0 +1,16 @@
+<?php
+define('APPLICATION_NAME','blossom');
+
+define('BASE_URI' , '/blossom');
+define('BASE_HOST', 'localhost');
+define('BASE_URL' , "http://".BASE_HOST.BASE_URI);
+
+define('DATE_FORMAT',    'n/j/Y');
+define('TIME_FORMAT',    'g:i a');
+define('DATETIME_FORMAT', DATE_FORMAT.' '.TIME_FORMAT);
+define('LOCALE', 'en_US');
+
+define('PAGINATION_ITEMS_PER_PAGE', 20);
+
+$DEFAULTS = [
+];
